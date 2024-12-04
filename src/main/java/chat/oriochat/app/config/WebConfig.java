adb7db8bd2031a891d078de1d1cd182786fc0efc
mamun-swe <mamun.swe.277@gmail.com>
2024-12-04 03:17:07 -0600

    CORS policay updated

--- src/main/java/chat/oriochat/app/config/WebConfig.java
+++ src/main/java/chat/oriochat/app/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://oriochat.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "http://4.204.71.173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
