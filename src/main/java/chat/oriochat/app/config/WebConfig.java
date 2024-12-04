3adad7ddf6109e7807b47eda4d493f59b0056e79
mamun-swe <mamun.swe.277@gmail.com>
2024-12-04 00:59:40 -0600

    Changes

--- src/main/java/chat/oriochat/app/config/WebConfig.java
+++ src/main/java/chat/oriochat/app/config/WebConfig.java
@@ -1,36 +1,17 @@
 package chat.oriochat.app.config;
 
-import chat.oriochat.app.filters.JwtTokenFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    // CORS configuration to allow cross-origin requests
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://oriochat.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://oriochat.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-
-    // Filter registration for JWT token validation
-    // @Bean
-    // public FilterRegistrationBean<JwtTokenFilter> filterFilterRegistration() {
-    //     FilterRegistrationBean<JwtTokenFilter> registrationBean = new FilterRegistrationBean<>();
-    //     registrationBean.setFilter(new JwtTokenFilter());
-    //     registrationBean.addUrlPatterns("/api/v1/*");
-    //     registrationBean.setOrder(1);
-    //     return registrationBean;
-    // }
 }
