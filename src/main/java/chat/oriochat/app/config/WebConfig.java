20594df294a4de8abab60fbfffd309e4756213b9
mamun-swe <mamun.swe.277@gmail.com>
2024-12-03 23:44:55 -0600

    CORS issue fixes

--- src/main/java/chat/oriochat/app/config/WebConfig.java
+++ src/main/java/chat/oriochat/app/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://oriochat.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
